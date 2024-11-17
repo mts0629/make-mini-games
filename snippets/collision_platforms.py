6ec114c53e5f3d62af9601af0ce69a15661cf058
mts0629 <soratobi93@gmail.com>
2024-11-17 23:58:42 +0900

    Collision platforms: try to simplify again

--- snippets/collision_platforms.py
+++ snippets/collision_platforms.py
@@ -1,7 +1,5 @@
 """Collision check between the player and platforms in a stage."""
 
-from enum import Flag, auto
-
 import pygame
 
 # Constants
@@ -9,109 +7,12 @@ import pygame
 FALLING_SPEED = 20
 # Maximum speed
 MAX_SPEED = 650
-
-
-class CollisionFlag(Flag):
-    """Collision flag of 2 rectangles for each direction."""
-
-    NONE = 0  # No collision
-    ON_BOTTOM = auto()
-    ON_TOP = auto()
-    ON_LEFT = auto()
-    ON_RIGHT = auto()
-    ON_BOTTOM_LEFT = ON_BOTTOM | ON_LEFT
-    ON_BOTTOM_RIGHT = ON_BOTTOM | ON_RIGHT
-    ON_TOP_LEFT = ON_TOP | ON_LEFT
-    ON_TOP_RIGHT = ON_TOP | ON_RIGHT
-
-
-def check_collision(target_rect, prev_target_rect, obj_rect):
-    """Check whether the collision area of a target collides to that of an objective.
-
-    Args:
-        target_rect (pygame.Rect): Collision area of the target.
-        prev_target_rect (pygame.Rect): Collision are of the target in the previous frame.
-        obj_rect (pygame.Rect): Collision of the objective.
-
-    Returns:
-        (CollisionFlag): Collision flag for each direction between the rectangles.
-    """
-    # Collision check by rectangles
-    #
-    #           Left     Right
-    #           :        :
-    # Top ..... +--------+
-    #           | target |
-    #           |     +--|-----+
-    # Bottom .. +--------+     |
-    #                 |  obj   |
-    #                 +--------+
-    #
-    on_top = obj_rect.top < target_rect.top <= obj_rect.bottom
-    on_bottom = obj_rect.top <= target_rect.bottom < obj_rect.bottom
-    on_left = obj_rect.left < target_rect.left <= obj_rect.right
-    on_right = obj_rect.left <= target_rect.right < obj_rect.right
-
-    if (not on_top) and on_bottom and on_left and on_right:
-        return CollisionFlag.ON_BOTTOM
-    elif on_top and (not on_bottom) and on_left and on_right:
-        return CollisionFlag.ON_TOP
-    elif on_top and on_bottom and on_left and (not on_right):
-        return CollisionFlag.ON_LEFT
-    elif on_top and on_bottom and (not on_left) and on_right:
-        return CollisionFlag.ON_RIGHT
-    elif (not on_top) and on_bottom and on_left and (not on_right):
-        return CollisionFlag.ON_BOTTOM | CollisionFlag.ON_LEFT
-    elif (not on_top) and on_bottom and (not on_left) and on_right:
-        return CollisionFlag.ON_BOTTOM | CollisionFlag.ON_RIGHT
-    elif on_top and (not on_bottom) and on_left and (not on_right):
-        return CollisionFlag.ON_TOP | CollisionFlag.ON_LEFT
-    elif on_top and on_bottom and (not on_left) and on_right:
-        return CollisionFlag.ON_TOP | CollisionFlag.ON_RIGHT
-
-    # Collision when the player slipped through the object between 2 frames
-    # Bottom
-    if (prev_target_rect.bottom <= obj_rect.top) and (
-        target_rect.bottom > obj_rect.top
-    ):
-        if on_left and on_right:
-            return CollisionFlag.ON_BOTTOM
-        elif on_left and (not on_right):
-            return CollisionFlag.ON_BOTTOM | CollisionFlag.ON_LEFT
-        elif (not on_left) and on_right:
-            return CollisionFlag.ON_BOTTOM | CollisionFlag.ON_RIGHT
-    # Top
-    if (prev_target_rect.top >= obj_rect.bottom) and (
-        target_rect.top < obj_rect.bottom
-    ):
-        if on_left and on_right:
-            return CollisionFlag.ON_TOP
-        elif on_left and (not on_right):
-            return CollisionFlag.ON_TOP | CollisionFlag.ON_LEFT
-        elif (not on_left) and on_right:
-            return CollisionFlag.ON_TOP | CollisionFlag.ON_RIGHT
-    # Left
-    if (prev_target_rect.left >= obj_rect.right) and (
-        target_rect.left < obj_rect.right
-    ):
-        if on_top and on_bottom:
-            return CollisionFlag.ON_LEFT
-        elif on_top and (not on_bottom):
-            return CollisionFlag.ON_TOP | CollisionFlag.ON_LEFT
-        elif (not on_top) and on_bottom:
-            return CollisionFlag.ON_BOTTOM | CollisionFlag.ON_LEFT
-    # Right
-    if (prev_target_rect.right <= obj_rect.left) and (
-        target_rect.right > obj_rect.left
-    ):
-        if on_top and on_bottom:
-            return CollisionFlag.ON_RIGHT
-        elif on_top and (not on_bottom):
-            return CollisionFlag.ON_TOP | CollisionFlag.ON_RIGHT
-        elif (not on_top) and on_bottom:
-            return CollisionFlag.ON_BOTTOM | CollisionFlag.ON_RIGHT
-
-    return CollisionFlag.NONE
+# Size of character block
+B_SIZE = 40
+# Player's speed
+PLAYER_SPEED_X = 200
+# Jump speed
+JUMP_SPEED = 650
 
 
 class Player:
@@ -138,11 +39,11 @@ class Player:
         """
         self.pos = pos.copy()
         self.v = pygame.Vector2(0, 0)
-        self.speed = 200
-        self.jumping_speed = 650
+        self.speed = PLAYER_SPEED_X
+        self.jumping_speed = JUMP_SPEED
         self.on_ground = False
-        self.width = 40
-        self.height = 40
+        self.width = B_SIZE
+        self.height = B_SIZE
         self.rect = pygame.Rect(
             self.pos.x - self.width / 2,
             self.pos.y - self.height / 2,
@@ -153,17 +54,22 @@ class Player:
 
         self.jump_key_is_holding = False
 
-    def move(self, screen, objectives, dt):
+    def move(self, objects, dt):
         """Move on the screen.
 
         Args:
-            screen (pygame.Surface): Drawing screen.
-            objectives (List[Block]): Objectives.
+            objects (List[Block]): objects.
             dt (float): Elapsed time[sec] from the previous frame.
         """
+        # Free fall
+        self.v.y += FALLING_SPEED
+        # Limit fall speed
+        if self.v.y > MAX_SPEED:
+            self.v.y = MAX_SPEED
+
         if self.on_ground:
             # If not in jumping, move a player by the key inputs
-            self.v.update(0, 0)
+            self.v.x = 0
 
             # Jump
             if pygame.key.get_pressed()[pygame.K_w]:
@@ -182,15 +88,6 @@ class Player:
                 self.v.x = -self.speed
             elif pygame.key.get_pressed()[pygame.K_d]:
                 self.v.x = self.speed
-        else:
-            # Free fall
-            self.v.y += FALLING_SPEED
-            # Limit fall speed
-            if self.v.y > MAX_SPEED:
-                self.v.y = MAX_SPEED
-
-        # Previous collision area
-        prev_rect = self.rect
 
         # Calculate a temporal position and a collision area
         pos = self.pos + self.v * dt
@@ -201,73 +98,68 @@ class Player:
             self.rect.height,
         )
 
-        self.on_ground = False
+        def hit_x(player, object):
+            """Check collision between two rectangles for x-axis.
+
+            Args:
+                player (pygame.Rect): Collision area of the player.
+                object (pygame.Rect): Collision of the object.
+
+            Returns:
+                (Tuple[bool]): Collision flag for two directions,
+                    [left, right] of the player.
+            """
+            on_left = object.left < player.left <= object.right
+            on_right = object.left <= player.right < object.right
+            return on_left, on_right
+        
+        def hit_y(player, object):
+            """Check collision between two rectangles for y-axis.
+
+            Args:
+                player (pygame.Rect): Collision area of the player.
+                object (pygame.Rect): Collision of the object.
+
+            Returns:
+                (Tuple[bool]): Collision flag for two directions,
+                    [top, bottom] on the player.
+            """
+            on_top = object.top < player.top <= object.bottom
+            on_bottom = object.top <= player.bottom < object.bottom
+            return on_top, on_bottom
 
         # Collision check
-        for objective in objectives:
-            obj_rect = objective.rect
+        for object in objects:
+            obj_rect = object.rect
 
-            flag = check_collision(rect, prev_rect, obj_rect)
-            if flag != CollisionFlag.NONE:
-                # Limit player's position and velocity
-                if flag == CollisionFlag.ON_BOTTOM:
-                    # From the upper side, stop on the object's top
-                    if self.v.y > 0:
-                        self.v.y = 0
-                        pos.y = obj_rect.top - self.height / 2
-                    if prev_rect.bottom <= obj_rect.top:
-                        self.on_ground = True
-                elif flag == CollisionFlag.ON_TOP:
-                    # From the lower side, stop on the object's bottom
-                    if self.v.y < 0:
-                        self.v.y = 0
-                        pos.y = obj_rect.bottom + self.height / 2
-                elif flag == CollisionFlag.ON_LEFT:
+            on_left, on_right = hit_x(rect, obj_rect)
+            on_top, on_bottom = hit_y(rect, obj_rect)
+
+            if (on_top or on_bottom) and (on_left or on_right):
+                if on_left and (not on_right):
                     # From the right side, stop on the object's right
                     if self.v.x < 0:
                         self.v.x = 0
                         pos.x = obj_rect.right + self.width / 2
-                elif flag == CollisionFlag.ON_RIGHT:
+                if on_right and (not on_left):
                     # From the left side, stop on the object's left
                     if self.v.x > 0:
                         self.v.x = 0
                         pos.x = obj_rect.left - self.width / 2
-                elif flag == CollisionFlag.ON_BOTTOM_LEFT:
-                    # From the upper-left side
-                    if (self.v.y > 0) and (rect.bottom > obj_rect.top):
+
+                if on_bottom and (not on_top):
+                    # From the upper side, stop on the object's top
+                    if self.v.y > 0:
                         self.v.y = 0
                         pos.y = obj_rect.top - self.height / 2
-                    if prev_rect.bottom <= obj_rect.top:
-                        self.on_ground = True
-                    if (self.v.x < 0) and (rect.left > obj_rect.right):
-                        self.v.x = 0
-                        pos.x = obj_rect.right + self.width / 2
-                elif flag == CollisionFlag.ON_BOTTOM_RIGHT:
-                    # From the upper-right side
-                    if (self.v.y > 0) and (rect.bottom > obj_rect.top):
-                        self.v.y = 0
-                        pos.y = obj_rect.top - self.height / 2
-                    if prev_rect.bottom <= obj_rect.top:
-                        self.on_ground = True
-                    if (self.v.x > 0) and (rect.right < obj_rect.left):
-                        self.v.x = 0
-                        pos.x = obj_rect.left - self.width / 2
-                elif flag == CollisionFlag.ON_TOP_LEFT:
-                    # From the lower-left side
-                    if (self.v.y < 0) and (rect.top < obj_rect.bottom):
+                if on_top and (not on_bottom):
+                    # From the lower side, stop on the object's bottom
+                    if self.v.y < 0:
                         self.v.y = 0
                         pos.y = obj_rect.bottom + self.height / 2
-                    if (self.v.x < 0) and (rect.left > obj_rect.right):
-                        self.v.x = 0
-                        pos.x = obj_rect.right + self.width / 2
-                elif flag == CollisionFlag.ON_TOP_RIGHT:
-                    # From the lower-right side
-                    if (self.v.y < 0) and (rect.top < obj_rect.bottom):
-                        self.v.y = 0
-                        pos.y = obj_rect.bottom + self.height / 2
-                    if (self.v.x > 0) and (rect.right < obj_rect.left):
-                        self.v.x = 0
-                        pos.x = obj_rect.left - self.width / 2
+
+                if (not on_top) and on_bottom and (on_left or on_right):
+                    self.on_ground = True
 
         # Update the positions
         self.pos.x = pos.x
@@ -344,23 +236,27 @@ def main():
         "[W] Jump/[A] Left/[D] Right/[ESC] Quit", True, (255, 255, 255)
     )
 
-    player = Player(pygame.Vector2(screen.get_width() / 2, screen.get_height() - 20))
+    player = Player(pygame.Vector2(screen.get_width() / 2, screen.get_height() - B_SIZE / 2))
 
     S_WIDTH = screen.get_width()
     S_HEIGHT = screen.get_height()
 
     # Platforms
     platforms = [
-        Block(pygame.Vector2(S_WIDTH / 2, -5), S_WIDTH, 10),  # Ceiling
-        Block(pygame.Vector2(S_WIDTH / 2, S_HEIGHT + 5), S_WIDTH, 10),  # Floor
-        Block(pygame.Vector2(-5, S_HEIGHT / 2), 10, S_HEIGHT),  # Left wall
-        Block(pygame.Vector2(S_WIDTH + 5, S_HEIGHT / 2), 10, S_HEIGHT),  # Right wall
+        # Celing
+        Block(pygame.Vector2(S_WIDTH / 2, -B_SIZE / 2), S_WIDTH, B_SIZE),
+        # Floor
+        Block(pygame.Vector2(S_WIDTH / 2, S_HEIGHT + B_SIZE / 2), S_WIDTH, B_SIZE),
+        # Left wall
+        Block(pygame.Vector2(-B_SIZE / 2, S_HEIGHT / 2), B_SIZE, S_HEIGHT),
+        # Right wall
+        Block(pygame.Vector2(S_WIDTH + B_SIZE / 2, S_HEIGHT / 2), B_SIZE, S_HEIGHT),
         # Platforms in different sizes
-        Block(pygame.Vector2(S_WIDTH / 2 - 200, S_HEIGHT - 300), 80, 10),
-        Block(pygame.Vector2(S_WIDTH / 2, S_HEIGHT - 150), 100, 5),
-        Block(pygame.Vector2(S_WIDTH / 2 + 200, S_HEIGHT - 300), 160, 20),
-        Block(pygame.Vector2(S_WIDTH / 2 + 200, S_HEIGHT - 50), 80, 100),
-        Block(pygame.Vector2(75, S_HEIGHT - 120), 150, 40),
+        Block(pygame.Vector2(S_WIDTH / 2 - 200, S_HEIGHT - 300), B_SIZE * 2, B_SIZE),
+        Block(pygame.Vector2(S_WIDTH / 2, S_HEIGHT - 150), 100, B_SIZE),
+        Block(pygame.Vector2(S_WIDTH / 2 + 200, S_HEIGHT - 300), B_SIZE * 4, B_SIZE),
+        Block(pygame.Vector2(S_WIDTH / 2 + 200, S_HEIGHT - 40), B_SIZE * 2, B_SIZE * 3),
+        Block(pygame.Vector2(B_SIZE, S_HEIGHT - 120), B_SIZE * 4, B_SIZE),
     ]
 
     clock = pygame.time.Clock()
@@ -376,7 +272,7 @@ def main():
         if pygame.key.get_pressed()[pygame.K_ESCAPE]:
             running = False
 
-        player.move(screen, platforms, dt)
+        player.move(platforms, dt)
 
         screen.blit(background, (0, 0))
 
@@ -387,7 +283,7 @@ def main():
 
         screen.blit(help_text, (5, 5))
 
-        # Debug info
+        # Print debug info
         debug_text = font.render(
             f"(x,y)=({player.pos.x:.0f},{player.pos.y:.0f}),"
             f"(vx,vx)=({player.v.x:.0f},{player.v.y:.0f}),"
